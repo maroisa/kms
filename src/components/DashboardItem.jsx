be92ba432f669cc620f7977d7ebe4d0f06eb3d5d
maroisa <ahmadroisul412@gmail.com>
2025-08-14 05:18:14 +0700

    add lazy loading

--- src/components/DashboardItem.jsx
+++ src/components/DashboardItem.jsx
@@ -1,7 +1,7 @@
 import { A } from "@solidjs/router";
 
 export default function DashboardItem(props){
-    return <A href={props.href} class="btn btn-outline btn-primary flex flex-col justify-center items-center min-h-28">
+    return <A href={props.href} class="btn btn-outline btn-primary flex flex-col justify-center items-center min-h-28" preload>
         {props.children}
         <p class="font-bold mt-2 text-xs sm:text-base">{props.name}</p>
     </A>
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,19 @@
+import { lazy } from "solid-js";
 import AuthLayout from "./components/AuthLayout.jsx";
-import NotFound404 from "./pages/404.jsx";
-import InternalError500 from "./pages/500.jsx";
 import App from "./pages/App.jsx";
-import Dashboard from "./pages/logged/Dashboard.jsx";
-import Profile from "./pages/logged/Profile.jsx";
-import Ptik from "./pages/logged/Ptik.jsx";
-import Login from "./pages/Login.jsx";
-import Logout from "./pages/Logout.jsx";
 
 export const routes = [
     { path: "/", component: App },
-    { path: "/login", component: Login },
-    { path: "/logout", component: Logout },
-    { path: "/500", component: InternalError500 },
-    { path: "*404", component: NotFound404 },
+    { path: "/login", component: lazy(() => import("./pages/Login.jsx")) },
+    { path: "/logout", component: lazy(() => import("./pages/Logout.jsx")) },
+    { path: "/500", component: lazy(() => import("./pages/500.jsx")) },
+    { path: "*404", component: lazy(() => import("./pages/404.jsx")) },
     {
         component: AuthLayout,
         children: [
-            { path: "/ptik", component: Ptik },
-            { path: "/dashboard", component: Dashboard },
-            { path: "/profile", component: Profile },
+            { path: "/ptik", component: lazy(() => import("./pages/logged/Ptik.jsx")) },
+            { path: "/dashboard", component: lazy(() => import("./pages/logged/Dashboard.jsx")) },
+            { path: "/profile", component: lazy(() => import("./pages/logged/Profile.jsx")) },
         ],
     },
 ];
