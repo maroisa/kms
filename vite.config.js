b2865a6f13eca19e41a86e227ec1293b8d8b3a6d
maroisa <ahmadroisul412@gmail.com>
2025-08-13 16:55:16 +0700

    fix vite config wrong configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
   build: {
